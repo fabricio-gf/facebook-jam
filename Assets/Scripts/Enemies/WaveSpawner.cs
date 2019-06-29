892f03058503cd66875a4c62f66f0329f1f1df78
Fabrício <fabricio-gf@hotmail.com>
2019-06-29 17:13:59 -0300

    Added enemy spawner

--- Assets/Scripts/Enemies/WaveSpawner.cs
+++ Assets/Scripts/Enemies/WaveSpawner.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int initialEnemyNumber = 2;
+    public int currentWave = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public Transform[] tiles;
+    List<Transform> freeTiles = new List<Transform>();
+
+    public void SpawnWave(){
+        freeTiles.Clear();
+        freeTiles.AddRange(tiles);
+        int randomIndex = 0;
+        for(int i = 0; i < initialEnemyNumber + Mathf.FloorToInt(Mathf.Pow(currentWave, (1f/3f))); i++){
+            randomIndex = Random.Range(0, freeTiles.Count);
+            //spawn at index;
+            freeTiles.RemoveAt(randomIndex);
+        }
     }
 }
