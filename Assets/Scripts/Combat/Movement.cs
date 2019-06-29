8090821240a4032bfe301f92353876f4de23a315
Gabriel Simmel Nascimento <galbrato@gmail.com>
2019-06-29 20:48:47 -0300

    Prototipo de de IA não testado

---
+++ Assets/Scripts/Combat/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Movement : MonoBehaviour{
+    bool isMoving;
+    Unidade Unidade;
+    GridManager grid;
+    // Start is called before the first frame update
+    void Start() {
+        Unidade = GetComponent<Unidade>();
+        grid = FindObjectOfType<GridManager>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (isMoving) {
+            if (Vector2.Distance(transform.position, transform.parent.position) > 0.1f) {
+                transform.position = Vector2.MoveTowards(transform.position, transform.parent.position, Unidade.MovementSpeed * Time.deltaTime);
+                //Debug.Log("Andando para " + transform.parent.name);
+            } else {
+                transform.localPosition = Vector3.zero;
+                isMoving = false;
+            }
+        }
+
+    }
+
+    public bool MoveTowards(Transform target) {
+        if (!isMoving) {
+            Transform newParent = grid.NextTile(transform.parent, target.parent);
+            if (newParent != null && newParent.childCount == 0) {
+                transform.SetParent(newParent);
+                //Debug.Log("Agora andando para o " + newParent.name);
+                isMoving = true;
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/StateMachine/Ataque.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ataque : StateMachineBehaviour{
+    Unidade myUnidade;
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        if (myUnidade == null) {
+            myUnidade = animator.GetComponent<Unidade>();
+        }
+        //setar a velocidade de reprodução de ataque;
+        animator.speed = stateInfo.length/myUnidade.AtaqueDelay;
+    }
+
+    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        
+    }
+
+    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        animator.speed = 1;
+        //Aplicar dano
+        myUnidade.Target.ApplyDamage(myUnidade.Damage);
+    }
+}
