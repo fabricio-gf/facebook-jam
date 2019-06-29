49f18a8bb2c46b9ddaab58a76e4f402da5764335
Fabrício <fabricio-gf@hotmail.com>
2019-06-29 12:02:16 -0300

    Added grid and unit

---
+++ Assets/Scripts/IsOccupied.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsOccupied : MonoBehaviour
+{
+    public bool Occupied = false;
+    public Unit UnitScript = null;
+}
