7dcc21e71f149881eafa6807112d0c1436bedd67
Fabrício <fabricio-gf@hotmail.com>
2019-06-29 17:04:56 -0300

    Added a lot of initial systems

---
+++ Assets/Scripts/Managers/EndManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndManager : MonoBehaviour
+{
+    
+    public GameObject DefeatWindow = null;
+    public GameObject VictoryWindow = null;
+
+    public void TriggerDefeat(){
+        DefeatWindow.SetActive(true);
+    }
+
+    public void TriggerVictory(){
+        VictoryWindow.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    Animator animator = null;
+    public bool isPaused = false;
+
+    void Awake(){
+        animator = GetComponent<Animator>();
+    }
+
+    void TogglePause(){
+        isPaused = !isPaused;
+        if(isPaused){
+            Time.timeScale = 0;
+            animator.SetTrigger("PauseOn");
+        }
+        else{
+            Time.timeScale = 1;
+            animator.SetTrigger("PauseOff");
+        }
+    }
+}
