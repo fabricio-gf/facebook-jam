1189cc5e1edce69982b4689f92cd08e6bd1c6b25
Fabrício <fabricio-gf@hotmail.com>
2019-06-29 15:23:45 -0300

    Added place unit

---
+++ Assets/Scripts/Unit/PlaceUnit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceUnit : MonoBehaviour
+{
+    public bool isMap = false;
+    public bool isBench = false;
+
+    public void Interact(){
+        GameObject obj = SelectedUnit.instance.selectedObject;
+        Unit unit = SelectedUnit.instance.unit;
+        if(isBench){
+            if(unit.inBench){
+                if(obj != null){
+                    obj.transform.SetParent(transform);
+                    obj.transform.position = transform.position;
+                }
+            }
+            else{
+                // delete object
+                //create icon
+            }
+        }
+        else if(isMap){
+            if(!unit.inBench){
+                //set position
+            }
+            else{
+                //delete icon
+                //create object
+            }
+        }
+    }
+}
--- Assets/Scripts/Unit/Unit.cs
+++ Assets/Scripts/Unit/Unit.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     public UnitAttributes attributes = null;
+    public bool inBench = false;
 
     // Start is called before the first frame update
     void Start()
